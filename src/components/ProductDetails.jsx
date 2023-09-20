6f46fdd2d15cc38b00d479034727df26de2daabd
Raihan Mahmud <raihanmahmudiut@gmail.com>
2023-09-20 15:12:36 +0600

    edited product details

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -83,108 +83,113 @@ function ProductDetails() {
 
 	// Render the product details when available
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-2 bg-gradient-to-t from-white via-ace0f9 to-white py-10 rounded-lg">
-			<div className="flex flex-col w-full md:w-4/6 sm:mt-6 sm:ml-[80px]">
-				{/* Breadcrumb */}
+		<div className="flex flex-col">
+			<div>
 				<Breadcrumb items={items} className="mb-8" />
-				<div>
-					<img
-						src={product.thumbnail}
-						alt="product-main-img"
-						className="rounded-md cursor-pointer hover:bg-primary-orange"
-					/>
-				</div>
-				<div className="flex-row w-full pt-4 flex bg-white">
-					{/* Thumbnails */}
-					<div className="hidden md:flex justify-between">
-						{limitedImages.map((image, index) => (
-							<img
-								key={index}
-								src={image}
-								alt="`Thumbnail ${index +1}`"
-								className="rounded-lg w-1/5 h-auto cursor-pointer hover:opacity-80"
-							/>
-						))}
-					</div>
-				</div>
 			</div>
-			<div className="flex flex-col justify-between px-10 py-8 ">
-				<h2 className="self-start text-slate-500 font-bold text-xl mb-4">
-					{product.brand}
-				</h2>
-				<div className=" self-start font-bold text-4xl mb-10">
-					{product.title}
-				</div>
-				<p className="self-start text-lg font-normal text-slate-500">
-					{product.description}
-				</p>
-				<div className="mt-6">
-					<div className="flex flex-col justify-between gap-4">
-						<div className="flex flex-row gap-2 h-6">
-							<p className=" self-center font-extrabold text-2xl">
-								${product.price}
-							</p>
-							<p className="self-center font-extrabold text-slate-500 ">
-								<span className="bg-slate-200 p-1 rounded-lg">
-									{product.discountPercentage}%
-								</span>
-							</p>
-						</div>
-						<div className="self-start text-slate-500 text-sm font-bold">
-							$
-							<del>
-								{parseFloat(
-									product.price +
-										(product.price * product.discountPercentage) / 100
-								).toFixed(2)}
-							</del>
+			<div className="grid grid-cols-1 md:grid-cols-2 bg-gradient-to-t from-white via-ace0f9 to-white py-10 rounded-lg">
+				<div className="flex flex-col w-full md:w-4/6 sm:mt-6 sm:ml-[80px]">
+					{/* Breadcrumb */}
+
+					<div>
+						<img
+							src={product.thumbnail}
+							alt="product-main-img"
+							className="rounded-md cursor-pointer hover:bg-primary-orange"
+						/>
+					</div>
+					<div className="flex-row w-full pt-4 flex bg-white">
+						{/* Thumbnails */}
+						<div className="hidden md:flex justify-between">
+							{limitedImages.map((image, index) => (
+								<img
+									key={index}
+									src={image}
+									alt="`Thumbnail ${index +1}`"
+									className="rounded-lg w-1/5 h-auto cursor-pointer hover:opacity-80"
+								/>
+							))}
 						</div>
 					</div>
 				</div>
-				<div className="flex flex-col md:flex-row gap-4 mt-8 justify-between items-center">
-					{/* Left Div */}
-					<div className="grid grid-cols-3 w-full md:w-1/3 items-center box-border border-slate-300 border-2 rounded-md">
-						<div className="cursor-pointer text-black hover:opacity-60 transition-opacity duration-[0.3s] text-2xl font-bold">
-							<PlusCircleFilled alt="" className="" />
-						</div>
-						<p className="text-black font-bold text-base text-center">1</p>
-						<div className="cursor-pointer text-black hover:opacity-60 transition-opacity duration-[0.3s] text-2xl font-bold">
-							<MinusCircleFilled alt="" className="" />
-						</div>
+				<div className="flex flex-col justify-between px-10 py-8 ">
+					<h2 className="self-start text-slate-500 font-bold text-xl mb-4">
+						{product.brand}
+					</h2>
+					<div className=" self-start font-bold text-4xl mb-10">
+						{product.title}
 					</div>
-
-					{/* Right Div */}
-
-					<div className="flex flex-row w-full md:w-2/3 justify-between gap-4 h-9">
-						<button
-							className="cursor-pointer w-11/12 border-slate-300 border-2  text-slate-800 rounded-md flex flex-row gap-1 justify-center items-center"
-							onClick={() => handleAddToCart(product)}
-						>
-							<ShoppingFilled className="md:text-xl" />
-							<p className="font-semibold">
-								Add to Cart
-								{cart[product.id] && cart[product.id] > 0 && (
-									<span style={{ marginLeft: "8px" }}>
-										({cart[product.id]})
+					<p className="self-start text-lg font-normal text-slate-500">
+						{product.description}
+					</p>
+					<div className="mt-6">
+						<div className="flex flex-col justify-between gap-4">
+							<div className="flex flex-row gap-2 h-6">
+								<p className=" self-center font-extrabold text-2xl">
+									${product.price}
+								</p>
+								<p className="self-center font-extrabold text-slate-500 ">
+									<span className="bg-slate-200 p-1 rounded-lg">
+										{product.discountPercentage}%
 									</span>
-								)}
-							</p>
-						</button>
+								</p>
+							</div>
+							<div className="self-start text-slate-500 text-sm font-bold">
+								$
+								<del>
+									{parseFloat(
+										product.price +
+											(product.price * product.discountPercentage) / 100
+									).toFixed(2)}
+								</del>
+							</div>
+						</div>
+					</div>
+					<div className="flex flex-col md:flex-row gap-4 mt-8 justify-between items-center">
+						{/* Left Div */}
+						<div className="grid grid-cols-3 w-full md:w-1/3 items-center box-border border-slate-300 border-2 rounded-md">
+							<div className="cursor-pointer text-black hover:opacity-60 transition-opacity duration-[0.3s] text-2xl font-bold">
+								<PlusCircleFilled alt="" className="" />
+							</div>
+							<p className="text-black font-bold text-base text-center">1</p>
+							<div className="cursor-pointer text-black hover:opacity-60 transition-opacity duration-[0.3s] text-2xl font-bold">
+								<MinusCircleFilled alt="" className="" />
+							</div>
+						</div>
 
-						<button
-							alt="Add to Wishlist"
-							style={{
-								display: "flex",
-								alignItems: "center",
-								justifyContent: "center",
-							}}
-							className=" border-slate-300 border-2 text-white rounded-md px-1 md:px-3"
-						>
-							<WishlistHeartIcon
-								inWishlist={isItemInWishlist(product.id)}
-								onClick={() => handleAddtoWishList(product)}
-							/>
-						</button>
+						{/* Right Div */}
+
+						<div className="flex flex-row w-full md:w-2/3 justify-between gap-4 h-9">
+							<button
+								className="cursor-pointer w-11/12 border-slate-300 border-2  text-slate-800 rounded-md flex flex-row gap-1 justify-center items-center"
+								onClick={() => handleAddToCart(product)}
+							>
+								<ShoppingFilled className="md:text-xl" />
+								<p className="font-semibold">
+									Add to Cart
+									{cart[product.id] && cart[product.id] > 0 && (
+										<span style={{ marginLeft: "8px" }}>
+											({cart[product.id]})
+										</span>
+									)}
+								</p>
+							</button>
+
+							<button
+								alt="Add to Wishlist"
+								style={{
+									display: "flex",
+									alignItems: "center",
+									justifyContent: "center",
+								}}
+								className=" border-slate-300 border-2 text-white rounded-md px-1 md:px-3"
+							>
+								<WishlistHeartIcon
+									inWishlist={isItemInWishlist(product.id)}
+									onClick={() => handleAddtoWishList(product)}
+								/>
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
