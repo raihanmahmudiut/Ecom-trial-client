49393cf2bf1b295151099d09c01994dc90fb44f0
Raihan Mahmud <raihanmahmudiut@gmail.com>
2023-09-21 18:57:51 +0600

    fixed horizontal scroll and searchbar for mobile

---
+++ src/components/lightBox.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LightBox() {
+	return (
+		<div className="hidden sm:block">
+			<div className="fixed inset">LightBox</div>
+		</div>
+	);
+}
+
+export default LightBox;
